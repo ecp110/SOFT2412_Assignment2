33b106a2f016d76a12feb4d98b648d5340bec96f
Ethan Proutt <eprou3000@uni.sydney.edu.au>
2021-10-15 21:10:17 +1100

    Main Page and Abstract Page setup part 1

--- app/src/main/java/movie/Page.java
+++ app/src/main/java/movie/Page.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Page {
+public abstract class Page {
     // Boolean status
     private boolean loggedIn = false;
     private boolean admin = false;
@@ -12,7 +12,10 @@ public class Page {
     private String creditCardLocation;
     private String giftCardLocation;
     private String usersLocation;
-    private static final String homePageString = "./database/homePage.txt";
+    public String homePageString;
+
+    // User information; stored in an arraylist of User objects
+    private ArrayList<User> users = new ArrayList<User>();
 
     public Page(String movieLocation, String cinemasLocation, String creditCardLocation, String giftCardLocation, String usersLocation) {
         this.movieLocation = movieLocation;
@@ -20,12 +23,56 @@ public class Page {
         this.creditCardLocation = creditCardLocation;
         this.giftCardLocation = giftCardLocation;
         this.usersLocation = usersLocation;
+
+        this.parseUsers();
     }
 
     /**
      * Displays the home / initial page which a user lands on for the given page
      */
-    public abstract String displayInitial() {}
+    public abstract String displayInitial();
+
+    /**
+     * What to do & display if at any time the user cancels the current operation
+     */
+    public abstract String cancel();
+
+    /** 
+     * Parses users into arraylist of users
+     */
+    private void parseUsers() {
+        // Initiates scanner for users file
+        try {
+            Scanner sc = new Scanner(new File("this.usersLocation"));
+            String[] line = new String[3];
+
+            // Skips the initial line with column names
+            sc.nextLine();
+            
+            boolean admin;
+            // Reads in each of the lines into User objects and stores them in the page
+            while (sc.hasNextLine()) {
+                admin = false;
+                line = sc.nextLine().split(",");
+
+                if (line[0] == null || line[0] == "") {
+                    break;
+                } else if (line[1] == null) {
+                    break;
+                } else if (line[2] == null) {
+                    break;
+                }
+
+                if (!(line[2].equals("customer"))) {
+                    admin = true;
+                }
+                this.users.add(new User(line[0], Integer.parseInt(line[1]), admin));
+            }
+
+        } catch (FileNotFoundException e) {
+            return;
+        }
+    }
 
     /**
      * Logs anyone in
@@ -39,7 +86,7 @@ public class Page {
             return false;
         }
         
-        // TODO: Write login function for users to login 
+        
         return true;
     }
 
