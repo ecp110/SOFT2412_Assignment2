8664165654056714a652f831743b40a41c80c53b
Ethan Proutt <eprou3000@uni.sydney.edu.au>
2021-11-03 11:36:28 +1100

    Complete Engine Reafctor

--- app/src/main/java/movie/Page.java
+++ app/src/main/java/movie/Page.java
@@ -237,6 +237,14 @@ public abstract class Page {
         return this.movies;
     }
 
+    public String displayInvalidInput() {
+        return "Sorry, that is not a valid input. Please try again.";
+    }
+
+    public String lineBreak() {
+        return "\n" + "------------------------------------------------------------------------" + "\n";
+    }
+
     //finds User object given username
     public String getUserByUsername(String username){
         //extracts all users from members.Json
@@ -337,6 +345,7 @@ public abstract class Page {
         while (i < len){
             if(allUsers.get(i).getName().toLowerCase().equals(username.toLowerCase())){
                 allUsers.remove(i);
+                break;
             }
             i += 1;
         }
